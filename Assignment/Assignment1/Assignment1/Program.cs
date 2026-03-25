0ab571855b20e14228dccefd53143681cc73943b
Vilvapriya Manivel <vilvapriyam@infinite.com>
2026-03-25 07:21:51 +0530

    assignment ans assesment folders are created

---
+++ Assignment/Assignment1/Assignment1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input 1st Number:");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input 2nd Number:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            if (n1 == n2)
+            {
+                Console.WriteLine("{0} and {1} are equal",n1,n2);
+            }
+            else {
+                Console.WriteLine("{0} and {1} are equal",n1,n2);
+                    }
+        }
+    }
+}
