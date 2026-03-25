f0c851964784e169276f6da48cc4376a3581e7d4
Vilvapriya Manivel <vilvapriyam@infinite.com>
2026-03-25 07:44:57 +0530

    first program is completed

--- Assignment/Assignment1/Assignment1/Program.cs
+++ Assignment/Assignment1/Assignment1/Program.cs
@@ -10,17 +10,38 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
+            //checkequal();
+            //checktype();
+        }
+        public static void checkequal()
+        {
             Console.Write("Input 1st Number:");
-            int n1=Convert.ToInt32(Console.ReadLine());
+            int n1 = Convert.ToInt32(Console.ReadLine());
             Console.Write("Input 2nd Number:");
             int n2 = Convert.ToInt32(Console.ReadLine());
             if (n1 == n2)
             {
-                Console.WriteLine("{0} and {1} are equal",n1,n2);
+                Console.WriteLine("{0} and {1} are equal", n1, n2);
             }
-            else {
-                Console.WriteLine("{0} and {1} are equal",n1,n2);
-                    }
+            else
+            {
+                Console.WriteLine("{0} and {1} are equal", n1, n2);
+            }
+        }
+        public static void checktype() {
+            Console.Write("Enter a number to check its type:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            if (n1 == 0)
+            {
+                Console.WriteLine("{0} is a nutral number", n1);
+            }
+            else if (n1 >= 1) {
+                Console.WriteLine("{0} is a positive number", n1);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a negative number", n1);
+            }
         }
     }
 }
