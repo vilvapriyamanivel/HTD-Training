4504af0104ce770f9e095e8ac46f70ba54d5299d
Vilvapriya Manivel <vilvapriyam@infinite.com>
2026-03-26 22:43:10 +0530

    Working on assignment 2

---
+++ Assignment/csharp/Assignment2/Assignment2/Assignment2/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+
+
+namespace Assignment2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            arravgminmax();
+            arrmarks();
+            arrcopy();
+            Strassignment strassignment = new Strassignment();
+            strassignment.wordlength();
+        }
+        public static void arravgminmax()
+        {
+            Console.Write("Enter array size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            Console.WriteLine("Enter Array elements :");
+            for (int j = 0; j < n; j++)
+            {
+                arr[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            int sum = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+                sum += arr[i];
+            }
+            int avg = sum / n;
+            Console.WriteLine($"The Minimum of the given Array is {min}");
+            Console.WriteLine($"The Maximum of the given Array is {max}");
+            Console.WriteLine($"The Avrage of the given Array is {avg}");
+        }
+        public static void arrmarks()
+        {
+            Console.Write("Enter number of subjects:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] marks = new int[n];
+            Console.WriteLine("Enter subject Marks :");
+            for (int j = 0; j < n; j++)
+            {
+                marks[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (marks[i] > max)
+                {
+                    max = marks[i];
+                }
+                if (marks[i] < min)
+                {
+                    min = marks[i];
+                }
+                sum += marks[i];
+            }
+            Console.WriteLine($"The Total marks is: {sum}");
+            Console.WriteLine($"The Averge marks is: {sum / n}");
+            Console.WriteLine($"The Minumum marks is: {min}");
+            Console.WriteLine($"The Maximum marks is: {max}");
+            for (int i = 0; i < n - 1; i++)
+            {
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (marks[i] > marks[j])
+                    {
+                        int temp = marks[i];
+                        marks[i] = marks[j];
+                        marks[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine($"The marks in Ascending order is:");
+            for (int k = 0; k < n; k++)
+            {
+                Console.WriteLine(marks[k]);
+            }
+            Console.WriteLine($"The marks in Descending order is:");
+            for (int k = n - 1; k >= 0; k--)
+            {
+                Console.WriteLine(marks[k]);
+            }
+
+        }
+        public static void arrcopy()
+        {
+            Console.Write("Enter array size:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr1 = new int[n];
+            Console.WriteLine("Enter Array elements :");
+            for (int j = 0; j < n; j++)
+            {
+                arr1[j] = Convert.ToInt32(Console.ReadLine());
+            }
+            int[] arr2 = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr2[i] = arr1[i];
+            }
+            Console.WriteLine($"The elements of the first array is:");
+            for (int k = 0; k < n; k++)
+            {
+                Console.WriteLine(arr1[k]);
+            }
+            Console.WriteLine($"The elements of the second array is:");
+            for (int k = 0; k < n; k++)
+            {
+                Console.WriteLine(arr2[k]);
+            }
+        }
+
+    }
+}
+
---
+++ Assignment/csharp/Assignment2/Assignment2/Assignment2/Stringquestions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+
+namespace Assignment2
+{
+    public class Strassignment
+    {
+        public static void Main(string[] args)
+        {
+            Strassignment str = new Strassignment();
+            str.wordlength();
+            str.reverseword();
+            str.sameword();
+        }
+        public void wordlength()
+        {
+            Console.Write("Enter a string:");
+            string s1 = Console.ReadLine();
+            int f = 0;
+            int l = s1.Length;
+            while (s1[f] == ' ')
+            {
+                f++;
+            }
+            while (s1[l - 1] == ' ')
+            {
+                l--;
+            }
+            int count = 0;
+            for (int i = f; i < l; i++)
+            {
+                count++;
+            }
+            Console.WriteLine($"The length of the string is {count}");
+        }
+
+        public void reverseword()
+        {
+            Console.Write("Enter a string:");
+            string s1 = Console.ReadLine();
+            for (int i = s1.Length - 1; i >= 0; i--)
+            {
+                Console.Write(s1[i]);
+            }
+        }
+        public void sameword()
+        {
+            Console.Write("Enter first string:");
+            string s1 = Console.ReadLine();
+            Console.Write("Enter second string:");
+            string s2 = Console.ReadLine();
+            if (s1 == s2)
+            {
+                Console.WriteLine("The two strings are same.");
+            }
+            else
+            {
+                Console.WriteLine("The two strings are different.");
+            }
+        }
+    }
+}
+
