da5cebad242585182f71aa6f4ea157a8b41ca25d
Vilvapriya Manivel <vilvapriyam@infinite.com>
2026-03-31 15:31:39 +0530

    Assignment3

---
+++ Assignment/csharp/Assignment3/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    public class Accounts
+    {
+        public int AccountNumber;
+        public string CustomerName;
+        public string AccountType;
+        public double Balance;
+        public Accounts(int AccounNumber, String CustomerName, String AccountType)
+        {
+            this.AccountNumber = AccounNumber;
+            this.CustomerName = CustomerName;
+            this.AccountType = AccountType;
+            Balance = 6000;
+
+        }
+        public void credit(double amount)
+        {
+            Balance += amount;
+
+        }
+        public void debit(double amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+
+            }
+            else
+            {
+                Console.WriteLine("Insufficient balance.");
+
+            }
+        }
+
+        public virtual void showData()
+        {
+            Console.WriteLine("Account Number: " + AccountNumber);
+            Console.WriteLine("Customer Name: " + CustomerName);
+            Console.WriteLine("Account Type: " + AccountType);
+            Console.WriteLine("Balance: " + Balance);
+        }
+    }
+    public class Transaction : Accounts
+    {
+        public char TransactionType;
+        public double Amount;
+        public Transaction(int accountNumber, string customerName, string accountType)
+                    : base(accountNumber, customerName, accountType)
+        {
+        }
+
+        public void TransactionProcessing()
+        {
+
+            if (TransactionType == 'w' || TransactionType == 'W')
+            {
+                debit(Amount);
+            }
+            else if (TransactionType == 'D' || TransactionType == 'd')
+            {
+                credit(Amount);
+            }
+            else
+            {
+                Console.WriteLine("Invalid transaction type.");
+            }
+        }
+
+        public override void showData()
+        {
+            base.showData();
+            Console.WriteLine("Transaction Type: " + TransactionType);
+            Console.WriteLine("Amount: " + Amount);
+        }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transaction transaction = new Transaction(12345, "John Doe", "Savings");
+            transaction.TransactionType = 'D';
+            transaction.Amount = 500.0;
+            transaction.TransactionProcessing();
+            transaction.showData();
+
+        }
+
+    }
+}
---
+++ Assignment/csharp/Assignment3/Student1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Student1
+    {
+        public int RollNo;
+        public string Name;
+        public string classofstudent;
+        public int Semester;
+        public string Branch;
+        int[] Marks = new int[5];
+        public Student1(int rollno, string name, string classofstudent, int semester, string branch)
+        {
+            this.RollNo = rollno;
+            this.Name = name;
+            this.classofstudent = classofstudent;
+            this.Semester = semester;
+            this.Branch = branch;
+            Console.WriteLine("Enter marks of 5 subjects:");
+            GetMarks();
+        }
+
+
+        public void GetMarks()
+        {
+            Marks[0] = Convert.ToInt16(Console.ReadLine());
+            Marks[1] = Convert.ToInt16(Console.ReadLine());
+            Marks[2] = Convert.ToInt16(Console.ReadLine());
+            Marks[3] = Convert.ToInt16(Console.ReadLine());
+            Marks[4] = Convert.ToInt16(Console.ReadLine());
+        }
+        public void Result()
+        {
+            int total = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                total += Marks[i];
+                if (Marks[i] < 35)
+                {
+                    Console.WriteLine("Sorry You Fail  But Don't Giveup");
+                    return;
+                }
+            }
+            int avg = total / 5;
+            for (int i = 0; i < 5; i++)
+            {
+                if (Marks[i] > 35 && avg < 50)
+                {
+                    Console.WriteLine("Sorry You Failed But  Don't Give up");
+                    return;
+                }
+            }
+            Console.WriteLine(" Congratulations");
+            Console.WriteLine(" You Passed!");
+
+        }
+
+
+    }
+    public class Stumain
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Roll No:");
+            int RollNo= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Name:");
+            string Name= Console.ReadLine();
+            Console.Write("Enter Class:");
+            string classofstudent= Console.ReadLine();
+            Console.Write("Enter Semester:");
+            int Semester = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Branch:");
+            string Branch= Console.ReadLine();
+
+             Student1 s1 = new Student1(RollNo, Name, classofstudent, Semester, Branch);
+            s1.Result();
+        }
+    }
+}
+
+
+
